46fe04346ead339f61d33c9334e06d4653ff6594
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2025-09-18 23:52:50 -0600

    feat: agregar configuración de Tailwind CSS

---
+++ public/js/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#5D5CDE',
+                sidebar: '#8B7EC7',
+                sidebarDark: '#7A6DB8'
+            }
+        }
+    }
+}
